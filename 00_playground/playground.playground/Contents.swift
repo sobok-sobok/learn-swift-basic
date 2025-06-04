afae2c640ff11cd9f27b8f41e8651584428656ca
jud.lee <jud.lee@kakaocorp.com>
2025-06-04 17:29:33 +0900

    01 hello world

---
+++ 00_playground/playground.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var a = 1
+var b = 2
+a = a + b
+
---
+++ 01_helloworld/helloworld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let label = UILabel()
+        label.text = "Hello world!"
+        label.textColor = .blue
+        label.backgroundColor = .yellow
+        label.font = UIFont.systemFont(ofSize: 50)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(label)
+
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor),
+        ])
+    }
+
+}
